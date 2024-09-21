dcb43a273eb6b28b205aa7a9195f46a934f3ac63
drewbob <122831351+drewbobO1@users.noreply.github.com>
2024-09-21 15:32:54 -0500

    feat: Add model and service components

---
+++ TvDbApp/src/app/shared/tv-detail.model.ts/tv-detail.model.ts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tv-detail.model.ts',
+  standalone: true,
+  imports: [],
+  templateUrl: './tv-detail.model.ts.component.html',
+  styles: ``
+})
+export class TvDetailModelTsComponent {
+
+}
---
+++ TvDbApp/src/app/shared/tv-detail.service.ts/tv-detail.service.ts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tv-detail.service.ts',
+  standalone: true,
+  imports: [],
+  templateUrl: './tv-detail.service.ts.component.html',
+  styles: ``
+})
+export class TvDetailServiceTsComponent {
+
+}
