6fd6c20d3c01f6412b17c3a2ad62f00c5a2ffcb1
drewbob <122831351+drewbobO1@users.noreply.github.com>
2024-09-22 21:52:22 -0500

    feat/style: Set up basic component hierarchy w/ imports etc. as well as make css files for each component manually (tut had me disable auto component level css so have to do it manually :(

--- TvDbApp/src/app/app.component.ts
+++ TvDbApp/src/app/app.component.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 
 // import { PrimeNGConfig } from 'primeng/api';
 import { FloatLabelModule } from 'primeng/floatlabel';
+import { TvDetailsComponent } from './tv-details/tv-details.component';
+import { TvDetailFormComponent } from './tv-details/tv-detail-form/tv-detail-form.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,9 @@ import { FloatLabelModule } from 'primeng/floatlabel';
   imports: [
     RouterOutlet,
     FormsModule,
-    FloatLabelModule
+    FloatLabelModule,
+    TvDetailFormComponent,
+    TvDetailsComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: [
--- TvDbApp/src/app/tv-details/tv-details.component.ts
+++ TvDbApp/src/app/tv-details/tv-details.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { TvDetailFormComponent } from './tv-detail-form/tv-detail-form.component';
+
 
 @Component({
   selector: 'app-tv-details',
   standalone: true,
-  imports: [],
+  imports: [
+    TvDetailFormComponent
+  ],
   templateUrl: './tv-details.component.html',
-  styles: ``
+  styleUrls: [
+    './tv-details.component.css'
+  ],
 })
 export class TvDetailsComponent {
 
