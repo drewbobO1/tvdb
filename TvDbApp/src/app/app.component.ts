d0fbb05810dda6ff22b114d093f74cff4919bd1a
drewbob <122831351+drewbobO1@users.noreply.github.com>
2024-09-22 22:16:46 -0500

    feat: Set up some basic PrimeNg components for the form entry fields

--- TvDbApp/src/app/app.component.ts
+++ TvDbApp/src/app/app.component.ts
@@ -1,20 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
 
 // import { PrimeNGConfig } from 'primeng/api';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { TvDetailsComponent } from './tv-details/tv-details.component';
-import { TvDetailFormComponent } from './tv-details/tv-detail-form/tv-detail-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    FormsModule,
+    // FormsModule,
     FloatLabelModule,
-    TvDetailFormComponent,
     TvDetailsComponent
   ],
   templateUrl: './app.component.html',
@@ -24,5 +22,4 @@ import { TvDetailFormComponent } from './tv-details/tv-detail-form/tv-detail-for
 })
 export class AppComponent {
   title = 'TvDbApp';
-  value: string = "";
 }
--- TvDbApp/src/app/tv-details/tv-detail-form/tv-detail-form.component.ts
+++ TvDbApp/src/app/tv-details/tv-detail-form/tv-detail-form.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputNumberModule } from 'primeng/inputnumber';
+
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+
 
 @Component({
   selector: 'app-tv-detail-form',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    FloatLabelModule,
+    InputNumberModule,
+    InputGroupModule,
+    InputGroupAddonModule
+  ],
   templateUrl: './tv-detail-form.component.html',
-  styles: ``
+  styleUrls: [
+    './tv-detail-form.component.css'
+  ],
 })
 export class TvDetailFormComponent {
-
+  title: string = "";
+  episodeQty: number = 0;
 }
