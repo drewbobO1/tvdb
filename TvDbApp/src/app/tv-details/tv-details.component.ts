cc53fea9fc7b695b8b0f889486492a4442b4a9c6
drewbob <122831351+drewbobO1@users.noreply.github.com>
2024-09-25 10:51:05 -0500

    feat: Move search form to dialog popover window

--- TvDbApp/src/app/tv-details/tv-details.component.ts
+++ TvDbApp/src/app/tv-details/tv-details.component.ts
@@ -5,6 +5,12 @@ import { OrderListModule } from 'primeng/orderlist';
 import { DataViewModule } from 'primeng/dataview';
 import { TvDetail } from '../shared/tv-detail.model';
 
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+
 
 @Component({
   selector: 'app-tv-details',
@@ -13,7 +19,11 @@ import { TvDetail } from '../shared/tv-detail.model';
     TvDetailFormComponent,
     CommonModule,
     OrderListModule,
-    DataViewModule
+    DataViewModule,
+    DialogModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    ButtonModule
   ],
   templateUrl: './tv-details.component.html',
   styleUrls: [
@@ -23,4 +33,9 @@ import { TvDetail } from '../shared/tv-detail.model';
 export class TvDetailsComponent {
 
   tvQueryList: any = [];
+  dialogVisible: boolean = false;
+
+  showDialog() {
+    this.dialogVisible = true;
+  }
 }
